8092a80833ad2179c02790d026035e779009e9b2
Peter Savage <silentkeystroke@googlemail.com>
2013-04-14 20:53:21 +0100

    Major refactoring

--- Index.py
+++ Index.py
@@ -35,98 +35,107 @@ class Index():
         if not os.listdir(self.wordIndex) == []:
             raise Error.ROIIndexError(Error.WordIndexNotEmpty)
         
-        for project in os.listdir(self.source):
-            self.indexProject(project)
+        for projectName in os.listdir(self.source):
+            projectInstance = self.Project(projectName, self.transIndex, self.wordIndex, self.packSize, self.source)
+            projectInstance.indexProject()
+
+    class Project():
+        def __init__(self, project, transIndex, wordIndex, packSize, source):
+            self.project = project
+            self.source = source
+            self.wordIndex = wordIndex
+            self.transIndex = transIndex
+            self.packSize = packSize
         
-    def indexProject(self, project):
-        self.windex = {}
-        self.id_database = {}
-        self.indexmeta = {}
-        self.index = 0
-
-        pos = ['en_GB.po','de.po','fr.po','as.po','az.po','be.po','cs.po','dz.po','ja.po']
+        def indexProject(self):
+            self.windex = {}
+            self.id_database = {}
+            self.indexmeta = {}
+            self.index = 0
+    
+            pos = ['en_GB.po','de.po','fr.po','as.po','az.po','be.po','cs.po','dz.po','ja.po']
+            
+            for pod in pos:
         
-        for pod in pos:
+                #self.indexProjectLanguage(pod, project)
+                self.indexmeta[pod] = {}
+                self.windex[pod] = {}
     
-            #self.indexProjectLanguage(pod, project)
-            self.indexmeta[pod] = {}
-            self.windex[pod] = {}
-
-            if not os.path.exists(self.transIndex+"/"+project+"/"+pod):
-                os.makedirs(self.transIndex+"/"+project+"/"+pod)
-            print pod
-            print self.source+"/"+project+"/"+pod
-            po = polib.pofile(self.source+"/"+project+"/"+pod)
-            pattern = re.compile('[^a-zA-Z0-9 ]+')
-            for entry in po.translated_entries():
-                # entry.msgid, entry.msgstr
-                sanitised = pattern.sub(' ', entry.msgstr)
-                words=pattern.sub('', sanitised).split(" ")
-
-                key = ",".join(map(lambda x:",".join(x), entry.occurrences))
-
-                if pod == "en_GB.po":
-                    self.id_database[key] = [self.index, entry.msgid]
-
-                for word in words:
-                    if len(word)>1:
-                        wlower = word.lower()
-                        pair = wlower[0:2]
-                        
-                        if not self.windex[pod].has_key(wlower[0:2]):
-                            self.windex[pod][wlower[0:2]] = {wlower:{project:[]}}
-                        else:
-                            if not self.windex[pod][wlower[0:2]].has_key(wlower):
-                                self.windex[pod][wlower[0:2]][wlower] = {project:[]}
-                        try:
-                            self.windex[pod][wlower[0:2]][wlower][project].append(self.id_database[key][0])
-                        except KeyError:
-                            continue
-
-                try:
-                    ourindex = str(self.id_database[key][0])
-                    self.indexmeta[pod][int(ourindex)] = entry.msgstr
-                except KeyError:
-                    continue
-
-                self.index += 1
+                if not os.path.exists(self.transIndex+"/"+self.project+"/"+pod):
+                    os.makedirs(self.transIndex+"/"+self.project+"/"+pod)
+                print pod
+                print self.source+"/"+self.project+"/"+pod
+                po = polib.pofile(self.source+"/"+self.project+"/"+pod)
+                pattern = re.compile('[^a-zA-Z0-9 ]+')
+                for entry in po.translated_entries():
+                    # entry.msgid, entry.msgstr
+                    sanitised = pattern.sub(' ', entry.msgstr)
+                    words=pattern.sub('', sanitised).split(" ")
     
-        for pod in self.indexmeta:
-            for p in range(0, len(self.indexmeta[pod]), self.packSize):
-                f = open(self.transIndex+"/"+project+"/"+pod+"/"+str(p),"w")
-                todump = {}
-                for a in range(p, p+self.packSize):
+                    key = ",".join(map(lambda x:",".join(x), entry.occurrences))
+    
+                    if pod == "en_GB.po":
+                        self.id_database[key] = [self.index, entry.msgid]
+    
+                    for word in words:
+                        if len(word)>1:
+                            wlower = word.lower()
+                            pair = wlower[0:2]
+                            
+                            if not self.windex[pod].has_key(wlower[0:2]):
+                                self.windex[pod][wlower[0:2]] = {wlower:{self.project:[]}}
+                            else:
+                                if not self.windex[pod][wlower[0:2]].has_key(wlower):
+                                    self.windex[pod][wlower[0:2]][wlower] = {self.project:[]}
+                            try:
+                                self.windex[pod][wlower[0:2]][wlower][self.project].append(self.id_database[key][0])
+                            except KeyError:
+                                continue
+    
                     try:
-                        todump[a] = self.indexmeta[pod][a]
+                        ourindex = str(self.id_database[key][0])
+                        self.indexmeta[pod][int(ourindex)] = entry.msgstr
                     except KeyError:
-                        if p<len(self.indexmeta[pod]):
-                            continue
-                        else:
-                            break
-                json.dump(todump, f)
-                f.close()
+                        continue
     
-        for pod in self.windex:
-            for pair in self.windex[pod]:
-                odir = self.wordIndex+"/"+pod+"/"+pair
-                if not os.path.exists(odir):
-                    os.makedirs(odir)
-    
-                if os.path.exists(self.wordIndex+"/"+pod+"/"+pair+"/"+pair):
-                    f = open(self.wordIndex+"/"+pod+"/"+pair+"/"+pair)
-                    old_windex = json.load(f)
+                    self.index += 1
+        
+            for pod in self.indexmeta:
+                for p in range(0, len(self.indexmeta[pod]), self.packSize):
+                    f = open(self.transIndex+"/"+self.project+"/"+pod+"/"+str(p),"w")
+                    todump = {}
+                    for a in range(p, p+self.packSize):
+                        try:
+                            todump[a] = self.indexmeta[pod][a]
+                        except KeyError:
+                            if p<len(self.indexmeta[pod]):
+                                continue
+                            else:
+                                break
+                    json.dump(todump, f)
                     f.close()
-    
-                    for word in self.windex[pod][pair]:
-                        if old_windex.has_key(word):
-                            old_windex[word][project] = self.windex[pod][pair][word][project]
-                        else:
-                            old_windex[word] = self.windex[pod][pair][word]
-                    
-                    f = open(self.wordIndex+"/"+pod+"/"+pair+"/"+pair, "w")
-                    json.dump(old_windex, f)
-                    f.close()
-                else:
-                    f = open(odir+"/"+pair, "w")
-                    json.dump(self.windex[pod][pair], f)
-                    f.close()
+        
+            for pod in self.windex:
+                for pair in self.windex[pod]:
+                    odir = self.wordIndex+"/"+pod+"/"+pair
+                    if not os.path.exists(odir):
+                        os.makedirs(odir)
+        
+                    if os.path.exists(self.wordIndex+"/"+pod+"/"+pair+"/"+pair):
+                        f = open(self.wordIndex+"/"+pod+"/"+pair+"/"+pair)
+                        old_windex = json.load(f)
+                        f.close()
+        
+                        for word in self.windex[pod][pair]:
+                            if old_windex.has_key(word):
+                                old_windex[word][self.project] = self.windex[pod][pair][word][self.project]
+                            else:
+                                old_windex[word] = self.windex[pod][pair][word]
+                        
+                        f = open(self.wordIndex+"/"+pod+"/"+pair+"/"+pair, "w")
+                        json.dump(old_windex, f)
+                        f.close()
+                    else:
+                        f = open(odir+"/"+pair, "w")
+                        json.dump(self.windex[pod][pair], f)
+                        f.close()
